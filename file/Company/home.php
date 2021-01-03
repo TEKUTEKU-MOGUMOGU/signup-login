bfbab34a8e97c58a3314bbdabd0ba7d1fb4e8213
kodai99 <69515273+kodai99@users.noreply.github.com>
2021-01-04 01:07:37 +0900

    Update home.php

--- file/Company/home.php
+++ file/Company/home.php
@@ -12,7 +12,6 @@ $_SESSION["token"] = base64_encode(openssl_random_pseudo_bytes(32));
 $token = $_SESSION["token"];
 
 // テーブル作成
-// delete();
 makePreRegistrationTable();
 makeRegistrationTable();
 
